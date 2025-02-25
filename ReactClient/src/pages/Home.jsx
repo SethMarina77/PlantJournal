d580a5a33f715260cd8ce485c8c2af54d05e316b
Seth Marina <Sethmarina53@gmail.com>
2025-02-25 14:53:51 -0500

    working on form to allow any summoner to be searched

--- ReactClient/src/pages/Home.jsx
+++ ReactClient/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   const fetchSummonerData = async () => {
     try {
-      const gameName = "HeNotBeatingGoku";
+      const gameName = "HeNotBeatingGoku"; // Hardcoded for now
       const tagLine = "THO";
       const response = await axios.get(
         `http://localhost:3000/api/summoner/${gameName}/${tagLine}`
@@ -19,9 +19,15 @@ const Home = () => {
       setSummonerData(null);
     }
   };
-
+ // This is the form that will be used to get the summoner data
   return (
     <div>
+      
+      <form>
+        <input type="text" placeholder="Game Name" /> 
+        <input type="text" placeholder="Tag Line" />
+      </form>
+
       <button onClick={fetchSummonerData}>Get Summoner Data</button>
       {error && <p style={{ color: "red" }}>Error: {error}</p>}
       {summonerData && (
